25902bec0252514f73cf70c376ce3ee397806ea9
diepdp <diepdp@kaiyouit.com>
2024-07-17 15:53:53 +0700

    update root config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cook.chiaser.project',
   appName: 'cook',
-  webDir: 'dist'
+  webDir: '.output/public'
 };
 
 export default config;
