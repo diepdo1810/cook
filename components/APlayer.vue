d6dfa6b54a77d374d2ec7cdc90a5b3013f796a29
diepdp <diepdp@kaiyouit.com>
2024-08-16 16:37:17 +0700

    feat: show dialog wiki

--- components/APlayer.vue
+++ components/APlayer.vue
@@ -3,119 +3,51 @@
 </template>
   
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import { useNuxtApp } from '#app';
 
-/**
-    async function fetchSongList() {
-        const ids = '1987660367,1977983298,2144492937,1955720485,1455452759,1943498394,1403213631,1479174227'
-        const response = await fetch(`https://chiaser-music.vercel.app/api/song/detail?ids=${ids}&realIP=211.161.244.70`);
-        const data = await response.json();
-        return data.songs.map(song => ({
-            id: song.id,
-            name: song.name,
-            artist: song.ar[0].name,
-            cover: song.al.picUrl,
-            theme: '#ebd0c2'
-        }));
-    }
 
-    async function fetchSongDetails(id) {
-        const response = await fetch(`https://chiaser-music.vercel.app/api/song/url?id=${id}&br=320000&realIP=211.161.244.70`);
-        const data = await response.json();
-        return data.data[0].url;
-    }
-
-    async function createAudioList() {
-        const songList = await fetchSongList();
-        const audioList = await Promise.all(songList.map(async song => {
-            // const url = await fetchSongDetails(song.id);
-            const localUrl = `/mp3/perfect_day_${song.id}.mp3`;
-            return {
-                id: song.id,
-                name: song.name,
-                artist: song.artist,
-                url: localUrl,
-                cover: song.cover,
-                lrc: 'lrc1.lrc',
-                theme: song.theme
-            };
-        }));
-
-        return audioList;
-    }
-
-    async function saveAudioListToLocalStorage() {
-        const audioList = await createAudioList();
-        localStorage.setItem('audio', JSON.stringify(audioList));
-    }
-
-    if (!localStorage.getItem('audio')) {
-        saveAudioListToLocalStorage();
-    }
-*/
-
-const lists = ref([]);
-
-const Audio = function(response, songList) {
-    this.response = response;
-    this.songList = songList;
+async function fetchSongList() {
+    const ids = '1987660367,1977983298,2144492937,1955720485,1455452759,1943498394,1403213631,1479174227'
+    const response = await fetch(`https://chiaser-music.vercel.app/api/song/detail?ids=${ids}&realIP=211.161.244.70`);
+    const data = await response.json();
+    return data.songs.map(song => ({
+        id: song.id,
+        name: song.name,
+        artist: song.ar[0].name,
+        cover: song.al.picUrl,
+        theme: '#ebd0c2'
+    }));
 }
 
-Audio.prototype.fetch = async function() {
-    this.response = await fetch(this.response);
-    return this.response;
+async function fetchSongDetails(id) {
+    const response = await fetch(`https://chiaser-music.vercel.app/api/song/url?id=${id}&br=320000&realIP=211.161.244.70`);
+    const data = await response.json();
+    return data.data[0].url;
 }
-
-Audio.prototype.getList = async function() {
-    const response = await this.fetch();
-    this.songList = await response.json();
-
-    return this.songList;
-}
-
-const songList = new Audio('/songs.json');
 
 async function createAudioList() {
-    const lists = await songList.getList();
-    const audioList = lists.map(song => ({
-        name: song.name,
-        artist: song.artist,
-        url: song.url,
-        cover: song.cover,
-        theme: song.theme
+    const songList = await fetchSongList();
+    const audioList = await Promise.all(songList.map(async song => {
+        const url = await fetchSongDetails(song.id);
+        return {
+            id: song.id,
+            name: song.name,
+            artist: song.artist,
+            url: url,
+            cover: song.cover,
+            lrc: 'lrc1.lrc',
+            theme: song.theme
+        };
     }));
 
     return audioList;
 }
 
-if (!localStorage.getItem('audio')) {
-    createAudioList().then(audioList => {
-        // randomize the list (save only 1 song)
-        const randomIndex = Math.floor(Math.random() * audioList.length);
-        const randomSong = audioList[randomIndex];
-        localStorage.setItem('audio', JSON.stringify([randomSong]));
-
-        /**
-        const dataStr = JSON.stringify(audioList);
-        const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
-        const exportFileDefaultName = 'audio.json';
-        const linkElement = document.createElement('a');
-        linkElement.setAttribute('href', dataUri);
-        linkElement.setAttribute('download', exportFileDefaultName);
-        linkElement.click();
-        */
-    });
-}
-
 onMounted(async () => {
     const id = document.getElementById('aplayer');
     const { $aplayer } = useNuxtApp();
-    const audioList = JSON.parse(localStorage.getItem('audio'));
-
-    if (!audioList) {
-        return;
-    }
+    const audioList = await createAudioList();
 
     new $aplayer({
         container: id,
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <script lang="ts" setup>
+import Cookies from 'js-cookie'
 const rStore = useRecipeStore()
+const showDialog = ref(false);
+const countdown = ref(15);
+let interval: NodeJS.Timeout | undefined;
+
+onMounted(() => {
+  const dialogShown = Cookies.get('dialogShown');
+
+  if (!dialogShown) {
+    showDialog.value = true;
+
+    interval = setInterval(() => {
+      countdown.value--;
+      if (countdown.value === 0) {
+        showDialog.value = false;
+        clearInterval(interval);
+        Cookies.set('dialogShown', 'true', { expires: 7 });
+      }
+    }, 1000);
+  }
+});
+
+onUnmounted(() => {
+  clearInterval(interval);
+});
+
 </script>
 
 <template>
@@ -17,6 +43,35 @@ const rStore = useRecipeStore()
     {{ $t('cookToday') }}
   </p>
   <ChooseFood />
+
+  <Dialog v-model:visible="showDialog" header="COOK" position="top" :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" :draggable="false" modal maximizable>
+    <p>Kính gửi quý khách,</p>
+    <br>
+    <p>Chúng tôi rất vui khi được đồng hành cùng bạn trong hành trình khám phá ẩm thực với ứng dụng Cook. 
+      Để giúp bạn có trải nghiệm tốt nhất, chúng tôi đã chuẩn bị một <a href="https://cook-wiki.vercel.app/" target="_blank" class="wiki">
+        tài liệu hướng dẫn chi tiết</a>, 
+      chứa đầy đủ thông tin và hướng dẫn cụ thể về cách sử dụng các tính năng của ứng dụng.</p>
+      <br>
+
+    <p>Chúng tôi gợi ý bạn hãy dành chút thời gian truy cập và 
+      <a href="https://cook-wiki.vercel.app/" target="_blank" class="wiki">tham khảo tài liệu hướng dẫn</a> 
+      để hiểu rõ hơn về những tính năng tuyệt vời mà Cook mang lại. 
+      Tài liệu này sẽ giúp bạn khai thác tối đa tiềm năng của ứng dụng và biến mỗi bữa ăn trở thành một trải nghiệm thú vị.</p>
+      <br>
+    <p>Cảm ơn bạn đã tin tưởng và sử dụng Cook!</p>
+
+    <p>Chúc bạn có những giây phút nấu ăn vui vẻ và thành công!</p>
+    <br>
+    <p>Tự động đóng sau {{ countdown }}s</p>
+  </Dialog>
   <!-- <ChooseFoodNew /> -->
 
 </template>
+
+<style scoped>
+.wiki {
+  color: #3b82f6;
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
